931d18484e3aa2247b6761a533eafe584c540a90
Abby Moe <161901430+amoe8802@users.noreply.github.com>
2024-09-09 10:07:52 -0400

    first commit

---
+++ src/com/coderscampus/assignment6/TeslaModels.java
@@ -0,0 +1,25 @@
+package com.coderscampus.assignment6;
+
+public enum TeslaModels {
+	MODEL_3, MODEL_S, MODEL_X;
+
+	@Override
+	public String toString() throws IllegalArgumentException {
+		switch (this) {
+		
+		case MODEL_3:
+			return "Model 3";
+		
+		
+		case MODEL_S:
+			return "Model S";
+		case MODEL_X:
+			return "Model X";
+		
+		default:
+			throw new IllegalArgumentException("Tesla model is in an invalid state.");
+		}
+
+	}
+
+}
